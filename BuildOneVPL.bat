088e68a7c1d921b75a2e5cd18fe1acf6511e3bfe
Ung, Teng En <teng.en.ung@intel.com>
2022-11-24 15:08:44 +0800

    Addin build and clean tasks and commands

--- BuildOneVPL.bat
+++ BuildOneVPL.bat
@@ -2,16 +2,17 @@
 if "%1"=="" goto end
 
 mkdir _build
+mkdir _build\%1
 mkdir _install
+mkdir _install\%1
 
-set VPL_INSTALL_DIR="%cd%\_install"
+set VPL_INSTALL_DIR="%cd%\_install\%1"
 
 set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;%PATH%
 
-cd _build
+cd _build\%1
 
-cmake ..\oneVPL -DCMAKE_INSTALL_PREFIX=%VPL_INSTALL_DIR%
-cmake --build . --config %1
+cmake ..\..\oneVPL -DCMAKE_INSTALL_PREFIX=%VPL_INSTALL_DIR%
 cmake --build . --config %1 --target install
 
 :end
